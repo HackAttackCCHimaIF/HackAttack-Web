9e845ff80378d483e896479d6e34f1b88508abbe
Rydhlnst <rydhlnst@gmail.com>
2025-10-27 11:04:58 +0700

    In god we trust

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,17 +15,10 @@ const koulen = Koulen({
   weight: ["400"],
 });
 
-<<<<<<< HEAD
 const playFair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-playfair",
   weight: ["400", "500", "600", "700", "800", "900"],
-=======
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair",
-  weight: ["400", "700", "900"],
->>>>>>> 6d50bfebcd4180261043d55f74ab1ff907568e63
 });
 
 export const metadata: Metadata = {
@@ -41,11 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-<<<<<<< HEAD
         className={`${poppins.variable} ${playFair.variable} ${koulen.variable} antialiased font-poppins `}
-=======
-        className={`${poppins.variable} ${koulen.variable} ${playfair.variable} antialiased font-poppins `}
->>>>>>> 6d50bfebcd4180261043d55f74ab1ff907568e63
       >
         <div>
           {children}
